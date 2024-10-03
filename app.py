e4d0ca04f35e65332b8be5dbf22845f0f426bd82
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2024-10-02 23:52:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def handle_update_time(data):
     global current_time
     current_time = round(data)
 
+
 @socketio.on('add_video')
 def handle_add_video(data):
 
@@ -41,6 +42,7 @@ def handle_add_video(data):
     }
     
     emit('update_queue', video_queue, broadcast=True)
+
 
 @socketio.on('song_ended')
 def handle_song_ended():
@@ -55,21 +57,6 @@ def handle_song_ended():
     video_queue = new_video_queue
     emit('update_queue', video_queue, broadcast=True)
 
-@socketio.on('song_error')
-def handle_song_error():
-    # Theory: song_ended does not go off, song error does
-    global video_queue
-    del video_queue[0]
-    del video_queue[0]
-    new_video_queue = {}
-    index = 0    
-    if len(video_queue) >= 1:
-        for key, value in video_queue.items():
-            new_video_queue[index] = value
-            index += 1
-    video_queue = new_video_queue
-    emit('resolve_error', video_queue, broadcast=True)   
 
 if __name__ == '__main__':
-    print('127.0.0.1:5000')
     socketio.run(app, debug=True)
