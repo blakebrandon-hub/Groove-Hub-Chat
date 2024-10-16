fbbfd25fcb14d28cfb49a837a82352f55297ca17
blakebrandon-hub <50201165+blakebrandon-hub@users.noreply.github.com>
2024-10-16 09:14:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ socketio = SocketIO(app, async_mode='gevent')
 video_queue = {}
 current_time = 0
 chat_messages = []
-current_video_index = 0
 
 @app.route('/')
 def landing_page():
@@ -26,25 +25,24 @@ def handle_message(msg):
         chat_messages.pop(0)  # Remove the oldest message (index 0)
 
     chat_messages.append(msg)  # Append the new message
-
     send(msg, broadcast=True)
 
 @socketio.on('connect')
 def handle_connect():
-    global current_video, current_time, currentVideoIndex
+    global current_video, current_time
 
     # When a new user joins, send them the current video and time
-    emit('sync_video', 
-        {'video_queue': video_queue, 
+    emit('sync_video', {
         'time': round(current_time), 
         'chat_messages': chat_messages,
-        "current_video_index": current_video_index});
+        'video_queue': video_queue})
 
 @socketio.on('update_time')
 def handle_update_time(data):
     global current_time
     current_time = round(data)
 
+# Add video to queue. Emit video queue. 
 @socketio.on('add_video')
 def handle_add_video(data):
 
@@ -56,10 +54,18 @@ def handle_add_video(data):
     
     emit('video_added', video_queue, broadcast=True)
 
-@socketio.on('song_ended')
-def handle_song_ended(index):
-    global video_queue, current_video_index
-    current_video_index = index
+# Delete first video and reindex queue. Emit on connect
+@socketio.on('video_ended')
+def handle_video_ended():
+    global video_queue
+    del video_queue[0]
+    new_video_queue = {}
+    index = 0
+    if len(video_queue) >= 1:
+        for key, value in video_queue.items():
+            new_video_queue[index] = value
+            index += 1
+    video_queue = new_video_queue
 
 
 if __name__ == '__main__':
