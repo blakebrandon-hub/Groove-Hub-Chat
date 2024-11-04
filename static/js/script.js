a38ab3d19bed5d1b2e47a9a4efe7efe95cbbc98e
Blake Brandon <50201165+blakebrandon-hub@users.noreply.github.com>
2024-11-04 10:37:49 -0500

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -415,6 +415,7 @@ document.addEventListener('visibilitychange', function() {
 
 // Manual Sync Button
 function manualSync() {
+     player.pauseVideo();
     // Emit a sync request to the server
     socket.emit('request_sync');
 }
